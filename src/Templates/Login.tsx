e18c61a263d191b17dec9d4783335adf993867bc
cynthia <abahojomacynthia@gmail.com>
2025-08-08 00:46:54 +0100

    changed from node-sass to sass

--- src/Templates/Login.tsx
+++ src/Templates/Login.tsx
@@ -1,6 +1,5 @@
 import { ChangeEvent, FC, useState } from 'react';
 import union from '../assets/Union.svg'
-
 import lensqr from '../assets/lendsqr.png'
 import { Link, useNavigate } from 'react-router-dom';
 import '../Styles/Login.scss'
